a7bc65a8d640c9779db81abc941eb242ddc076f3
fareed tijani <fareedtijani2810@gmail.com>
2024-05-13 08:51:21 -0700

    commit

---
+++ gateOne/day_six/java/MyAgeTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyAgeTest{
+	@Test
+	public void testThatMyAgeIsWorking() {
+
+	int result = MyAge. myAge(28-10-2007);
+
+	int expectedResult = 17;
+	//assertion
+	assertEquals(expectedResult, result);
+	}
+
+
+
+}
